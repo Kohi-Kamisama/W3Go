222c2ae92ca14821c6ba410c1d6e735005c27f98
Kohi-Kamisama <coffeemail48@gmail.com>
2025-09-02 08:46:09 -0700

    Go Comments

---
+++ Comments.go
@@ -0,0 +1,22 @@
+package main
+
+// This is a comment
+import (
+	"fmt"
+)
+
+func main() {
+	// This is a comment
+	fmt.Println("Comments!") // This is a comment
+	/*
+		This is a multi-line comment
+		Great for long comments
+		and useful for code blocks
+	*/
+}
+
+/*
+func comment(){
+	fmt.Println("This will not run.")
+}
+*/
