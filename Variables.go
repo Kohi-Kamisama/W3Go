78d2b6a0323fca9010bd4ff51fee774c34161fcf
Kohi-Kamisama <coffeemail48@gmail.com>
2025-09-03 13:19:07 -0700

    Declearing Variables

--- Variables.go
+++ Variables.go
@@ -4,7 +4,19 @@ import (
 	"fmt"
 )
 
-func Variables() { // Variables
+var d int
+var e int = 2
+var f = 3
+
+/*
+g := 4
+	":=" can only be used in a function the code above will produce an error
+*/
+
+func main() { // Variables
+	dash := "--------"
+	fmt.Println(dash)
+
 	var student1 string = "John" //type is string
 	var student2 = "Jane"        // type is inferred
 	x := 2                       //type is inferred
@@ -13,4 +25,28 @@ func Variables() { // Variables
 	fmt.Println(student2)
 	fmt.Println(x)
 
+	fmt.Println(dash)
+
+	var a string
+	var b int
+	var c bool
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+	fmt.Println(dash)
+
+	var student3 string
+	student3 = "Johnathon"
+	fmt.Println(student3)
+
+	fmt.Println(dash)
+
+	d = 1
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+
+	fmt.Println(dash)
 }
