abc09c9d6acb2ac188a43c1a71a1e4abf0b3c918
Kohi-Kamisama <coffeemail48@gmail.com>
2025-09-03 21:59:07 -0700

    Go Constants

--- Constants.go
+++ Constants.go
@@ -4,11 +4,42 @@ import (
 	"fmt"
 )
 
-func Constants() { // Constants
+func main() { // Constants
 	dash := "--------"
 	fmt.Println(dash)
 
-	// Use to copy and past, from f to the next line
-	fmt.Println()
+	const PI = 3.14159265
+	fmt.Println(PI)
+	// Constants usually typed in uppercase for easy identification, also differntation from variables. Varibales use Pascal case
+
+	fmt.Println(dash)
+
+	const A int = 1
+	fmt.Println(A)
+
+	fmt.Println(dash)
+
+	/*
+		const B = 3
+		B = 4
+		fmt.Println(B)
+
+			once you declare a constant you cannot chang it.
+			The reult would be a message: cannot assighn to B
+	*/
+
+	const (
+		C int = 9
+		D     = 3.1415
+		E     = "Hello World!"
+	)
+
+	fmt.Println(C)
+	fmt.Println(D)
+	fmt.Println(E)
+
+	fmt.Println(dash)
+	// Use to copy and past, from f to the next line. Comment out when following the examples.
+	//fmt.Println()
 
 }
