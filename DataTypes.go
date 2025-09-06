9cd32e5cb4cd621ff8c61cc83a7a9393ecad10af
Kohi-Kamisama <coffeemail48@gmail.com>
2025-09-06 11:19:49 -0700

    Boolean

--- DataTypes.go
+++ DataTypes.go
@@ -20,6 +20,18 @@ func main() { // DataTypes
 
 	fmt.Println(dash)
 
+	var b1 bool = true // typed declartion with initial value
+	var b2 = true      // untyped declartion with initial value
+	var b3 bool        // typed declartion without initial value
+	b4 := true         // untyped declartion with initial value
+
+	fmt.Println("b1: ", b1, " : typed declartion with initial value")
+	fmt.Println("b2: ", b2, " : untyped declartion with initial value")
+	fmt.Println("b3: ", b3, ": typed declartion without initial value")
+	fmt.Println("b4: ", b4, " : untyped declartion with initial value")
+
+	fmt.Println(dash)
+
 	// Use to copy and past, from f to the next line. Comment out when following the examples.
 	// fmt.Println()
 
