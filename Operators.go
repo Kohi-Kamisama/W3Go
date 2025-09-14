19702e7c7131757cf55f7178e421911eaf99f934
Kohi-Kamisama <coffeemail48@gmail.com>
2025-09-13 22:22:51 -0700

    Arithmetic

--- Operators.go
+++ Operators.go
@@ -4,11 +4,30 @@ import (
 	"fmt"
 )
 
-func Operators() { // Operators
+func main() { // Operators
 	dash := "--------"
 	fmt.Println(dash)
 
+	x := 8
+	y := 4
+
+	fmt.Print("Arithmetic Operators\n\n")
+
+	fmt.Println("Addition: 8+4 =", x+y)
+	fmt.Println("Subtraction: 8-4 =", x-y)
+	fmt.Println("Multiplication: 8*4 =", x*y)
+	fmt.Println("Division: 8/4 =", x/y)
+	fmt.Println("Modulus: 8%4 =", x%y)
+
+	x++
+	fmt.Println("Increment: 8++ =", x)
+
+	y--
+	fmt.Println("Decrement: 4-- =", y)
+
+	fmt.Println(dash)
+
 	// Use to copy and past, from f to the next line. Comment out when following the examples.
-	fmt.Println()
+	//fmt.Println()
 
 }
