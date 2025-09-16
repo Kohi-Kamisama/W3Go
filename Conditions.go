b3730bb57193edc53b7a8fa3ef9f180e78ab4d55
Kohi-Kamisama <coffeemail48@gmail.com>
2025-09-16 09:37:03 -0700

    if statements

--- Conditions.go
+++ Conditions.go
@@ -8,6 +8,19 @@ func Conditions() { // Conditions
 	dash := "--------"
 	fmt.Println(dash)
 
+	if 20 > 18 {
+		fmt.Println("20 is grater than 18 ")
+	}
+
+	a := 20
+	b := 18
+
+	if a > b {
+		fmt.Println("a is greater than b")
+	}
+
+	fmt.Println(dash)
+
 	// Use to copy and past, from f to the next line. Comment out when following the examples.
 	fmt.Println()
 
