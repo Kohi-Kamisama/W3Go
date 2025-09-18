25bec64f0021b97097dcde3c10b871cf72d4f541
Kohi-Kamisama <coffeemail48@gmail.com>
2025-09-18 11:19:34 -0700

    Switch

--- Conditions.go
+++ Conditions.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func main() { // Conditions
+func Conditions() { // Conditions
 	dash := "--------"
 	fmt.Println(dash)
 
--- Switch.go
+++ Switch.go
@@ -8,7 +8,63 @@ func Switch() { // Switch
 	dash := "--------"
 	fmt.Println(dash)
 
+	day := 4
+
+	switch day {
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+	case 3:
+		fmt.Println("Wednesday")
+	case 4:
+		fmt.Println("Thursday")
+	case 5:
+		fmt.Println("Friday")
+	case 6:
+		fmt.Println("Saturday")
+	case 7:
+		fmt.Println("Sunday")
+	}
+
+	day = 8
+
+	switch day {
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+	case 3:
+		fmt.Println("Wednesday")
+	case 4:
+		fmt.Println("Thursday")
+	case 5:
+		fmt.Println("Friday")
+	case 6:
+		fmt.Println("Saturday")
+	case 7:
+		fmt.Println("Sunday")
+	default:
+		fmt.Println("Not a weekday")
+	}
+
+	fmt.Println(dash)
+
+	day = 5
+
+	switch day {
+	case 1, 3, 5:
+		fmt.Println("Odd weekday")
+	case 2, 4:
+		fmt.Println("Even weekday")
+	case 6, 7:
+		fmt.Println("Weeken")
+	default:
+		fmt.Println("Invalid day")
+
+	}
+	fmt.Println(dash)
 	// Use to copy and past, from f to the next line. Comment out when following the examples.
-	fmt.Println()
+	//fmt.Println()
 
 }
