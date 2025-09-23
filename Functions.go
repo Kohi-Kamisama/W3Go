2118ccb981f02b8ff65fb9e499c29345792733ec
Kohi-Kamisama <coffeemail48@gmail.com>
2025-09-23 11:42:33 -0700

    Parameters/Arguments

--- Functions.go
+++ Functions.go
@@ -14,6 +14,22 @@ func main() { // Functions
 
 	fmt.Println(dash)
 
+	// This part I'm modifying the example to use what I learned about arrays and loops
+
+	names := [4]string{"Micheal", "Jack", "Luna", "Ana"}
+	for n := range names {
+		FamilyName(names[n])
+	}
+
+	fmt.Println("")
+
+	age := [4]int{42, 24, 23, 1}
+	for a := range age {
+		FamilyAge(names[a], age[a])
+	}
+
+	fmt.Println(dash)
+
 	// Use to copy and past, from f to the next line. Comment out when following the examples.
 	//fmt.Println()
 
@@ -21,4 +37,12 @@ func main() { // Functions
 
 func Message() {
 	fmt.Println("Gods Die")
+}
+
+func FamilyName(name string) {
+	fmt.Println("Welcome", name, "Hana")
+}
+
+func FamilyAge(name string, age int) {
+	fmt.Println(name, "Hana is", age, "Years Old")
 }
