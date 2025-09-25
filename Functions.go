97d5b3c6875aca814718e509228f56c804786bfc
Kohi-Kamisama <coffeemail48@gmail.com>
2025-09-24 23:17:46 -0700

    Functions returns

--- Functions.go
+++ Functions.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 )
 
+var x int = 8
+var y int = 4
+
 func main() { // Functions
 	dash := "--------"
 	fmt.Println(dash)
@@ -30,6 +33,23 @@ func main() { // Functions
 
 	fmt.Println(dash)
 
+	fmt.Println(Add(x, y))
+	fmt.Println(Sub(x, y))
+	fmt.Println(Div(x, y))
+	multiply := Mul(x, y)
+	fmt.Println(multiply)
+	fmt.Println("")
+	fmt.Println(MRV(y, "Gods"))
+	n1, t1 := MRV(x, "Devil")
+	fmt.Println(n1, t1)
+	fmt.Println("")
+	_, t2 := MRV(x, "Gods")
+	fmt.Println(t2)
+	n3, _ := MRV(x, "Devil")
+	fmt.Println(n3)
+
+	fmt.Println(dash)
+
 	// Use to copy and past, from f to the next line. Comment out when following the examples.
 	//fmt.Println()
 
@@ -45,4 +65,29 @@ func FamilyName(name string) {
 
 func FamilyAge(name string, age int) {
 	fmt.Println(name, "Hana is", age, "Years Old")
+}
+
+func Add(x int, y int) int {
+	return x + y
+}
+
+func Sub(x int, y int) (result int) {
+	result = x - y
+	return
+}
+
+func Div(x int, y int) (result int) {
+	result = x / y
+	return result
+}
+
+func Mul(x int, y int) (result int) {
+	result = x * y
+	return result
+}
+
+func MRV(i int, s string) (dub int, txt string) {
+	dub = i + i
+	txt = s + " Realm"
+	return
 }
