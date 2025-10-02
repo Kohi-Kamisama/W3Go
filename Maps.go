6da579c9474582ec2dcf212e3b435ebaedd3877c
Kohi-Kamisama <coffeemail48@gmail.com>
2025-10-01 22:08:39 -0700

    start of Maps

--- Maps.go
+++ Maps.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 )
 
-func Maps() { // Maps
+func main() { // Maps
 	dash := "--------"
 	fmt.Println(dash)
 
+	var c1 = map[string]string{"brand":"Ford", "model":"mustang", "year":"1964"}
+	fmt.Println(c1)
+
+	fmt.Println(dash)
 	// Use to copy and past, from f to the next line. Comment out when following the examples.
-	fmt.Println()
+	//fmt.Println()
 
 }
